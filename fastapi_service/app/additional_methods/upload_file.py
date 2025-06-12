5c85c4a7724ada481569bffbb8815952e72cc97c
MontaGGGne <shchukinaa2002@gmail.com>
2025-06-12 15:36:27 +0300

    Shchukin: fix

--- fastapi_service/app/additional_methods/upload_file.py
+++ fastapi_service/app/additional_methods/upload_file.py
@@ -32,7 +32,7 @@ async def upload_json(file: UploadFile, s3_full_path: str):
         raise HTTPException(
             status_code=HTTP_500_INTERNAL_SERVER_ERROR,
             detail=(f"REPR: {repr(e)}\n",
-                    f"DOTENV_INFO_1: {DOTENV_INFO_1}\nDOTENV_INFO_1: {DOTENV_INFO_2}\n",
+                    f"DOTENV_INFO_1: {DOTENV_INFO_1}\nDOTENV_INFO_2: {DOTENV_INFO_2}\n",
                     f"FULL_PATH: {FULL_PATH}\nUpload file go wrong: {traceback.format_exc()}")
         )
     finally:
